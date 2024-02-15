465cf34ae71c3d51854956b0013e2006e4b353fa
Jaylen Smith <kobe26945@icloud.com>
2024-02-15 16:09:19 -0500

    Initial Commit

---
+++ AccessBSU/AccessBSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessBSUApp.swift
+//  AccessBSU
+//
+//  Created by Jaylen Smith on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessBSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
